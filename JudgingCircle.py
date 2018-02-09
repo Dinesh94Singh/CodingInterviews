ef08e3aa35a30854ed18f9143a26afbe5f616292
Dinesh94Singh <Dinesh94Singh@gmail.com>
2018-02-09 10:07:27 -0600

    2/8/18

---
+++ JudgingCircle.py
@@ -0,0 +1,26 @@
+"""
+Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
+
+The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). The output should be true or false representing whether the robot makes a circle.
+
+Example 1:
+
+Input: "UD"
+Output: true
+
+Example 2:
+
+Input: "LL"
+Output: false
+
+
+Refer   1: https://www.tutorialspoint.com/python3/string_count.htm
+"""
+
+class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
