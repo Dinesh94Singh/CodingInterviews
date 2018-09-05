d480fa46715d927087a3e909b92938f37638090e
Savya Ananda <savyaananda@Savyas-MacBook-Pro.local>
2018-09-04 21:48:33 -0500

    Added flipANdInvertImage, morseCode files.

---
+++ morse_code.py
@@ -0,0 +1,48 @@
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+# Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+# Return the number of different transformations among all words we have.
+
+# Example:
+# Input: words = ["gin", "zen", "gig", "msg"]
+# Output: 2
+# Explanation: 
+# The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+
+# There are 2 different transformations, "--...-." and "--...--.".
+ 
+
+# Note:
+
+# The length of words will be at most 100.
+# Each words[i] will have length in range [1, 12].
+# words[i] will only consist of lowercase letters.
+
+class Solution:
+    def getMorse(self, word):
+        translation = str()
+        for ch in word:
+            translation += self.translations[ord(ch) - 97]
+        
+        return translation
+    
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        self.translations = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        transforms = set()
+        
+        for word in words:
+            transforms.add(self.getMorse(word))
+        
+        return len(transforms)
